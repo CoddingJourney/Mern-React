84d161246b2deb9ae32fd986e33f17bd5e7b1480
Bikram Gyawali <gyawalibikram0@gmail.com>
2024-10-01 22:47:23 -0800

    Complete Card

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
+import Card from '../component/Card'
 
 const Home = () => {
     return (
         <>
             <Navbar />
+
+
+
+            <div className=' flex flex-wrap mt-16'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </>
     )
 }
